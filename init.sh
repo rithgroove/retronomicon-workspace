0d283f436202283406e1eef8abaa4bed8fe4ecdd
rithgroove <rithgroove@gmail.com>
2025-07-05 09:27:12 +0700

    update init to load platformer engine

--- init.sh
+++ init.sh
@@ -1,9 +1,22 @@
 if [ ! -e "retronomicon" ]; then
     git clone git@github.com:rithgroove/retronomicon.git
+    cd retronomicon
+    USE_LUA=ON USE_PYTHON=ON  ./init.sh
+    cd ..
 elif [[ ! -d "retronomicon" ]]; then
-    echo "$dir already exists but is not a directory" 
+    echo "retronomicon already exists but is not a directory" 
 else
     cd retronomicon
+    git pull
+    cd ..
+fi
+
+if [ ! -e "retronomicon-platformer" ]; then
+    git clone git@github.com:rithgroove/retronomicon-platformer.git
+elif [[ ! -d "retronomicon-platformer" ]]; then
+    echo "retronomicon-platformer already exists but is not a directory" 
+else
+    cd retronomicon-platformer
     git pull
     cd ..
 fi
