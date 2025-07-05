c161b14f13c0b81fbe47d024d57adb95ab14bd2d
rithgroove <rithgroove@gmail.com>
2025-07-05 11:11:24 +0700

    remove asset from this repository and move it to demo

--- git_push.sh
+++ git_push.sh
@@ -12,7 +12,6 @@ if [ ! -e "retronomicon-platformer" ]; then
     echo "no retronomicon-platformer directory" 
 else
     cd retronomicon-platformer
-    git pull
     git add -A
     git commit
     git push
@@ -23,9 +22,13 @@ if [ ! -e "retronomicon-platformer-demo" ]; then
     echo "no retronomicon-platformer-demo directory" 
 else
     cd retronomicon-platformer-demo
-    git pull
     git add -A
     git commit
     git push
     cd ..
 fi
+
+
+git add -A
+git commit
+git push 
