9c21b4adff73408a277581a12a5e27649fc52579
rithgroove <rithgroove@gmail.com>
2025-07-13 10:03:10 +0700

    update main cpp to test to_string method

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -8,8 +8,8 @@ int main() {
     Point pos(10.0f, 20.0f);
     Rect rect(pos, 100.0f, 50.0f);
 
-    std::cout << "Rect Position: (" << rect.getX() << ", " << rect.getY() << ")\n";
-    std::cout << "Rect Size: " << rect.getWidth() << "x" << rect.getHeight() << std::endl;
+    std::cout << pos << "\n";
+    std::cout << rect << std::endl;
 
     // Initialize SDL
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
