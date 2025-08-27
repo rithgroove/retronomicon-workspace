7b8911843291a9638e0648ea86fb1234a9e40dee
rithgroove <rithgroove@gmail.com>
2025-08-27 07:53:38 +0700

    cannot do much, today I add library to install in init.sh because I have to go to the office

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
 git submodule update --init --recursive
 
+sudo apt-get install libsdl2-ttf-dev 
 sudo apt-get install libfreetype6-dev
+sudo apt-get install libxext-dev
+sudo apt-get install libogg-dev libvorbis-dev
